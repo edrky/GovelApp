a63d6158b7fa8b8d03e38b573baa55eb576db662
alevnyaa <alevnyaa@gmail.com>
2016-11-20 05:19:21 +0300

    Yep, gradle seems to be working fine. Barebones xml for main activity done. App launches!

--- app/src/main/java/com/govelapp/govelapp/MainActivity.java
+++ app/src/main/java/com/govelapp/govelapp/MainActivity.java
@@ -2,21 +2,22 @@ package com.govelapp.govelapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    final Button;
-    final EditText;
+    private Button button;
+    private EditText text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.search_button);
-        EditText text = (EditText)findViewById(R.id.search_input);
+        button = (Button) findViewById(R.id.search_button);
+        text = (EditText)findViewById(R.id.search_input);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String queryString = text.getText().toString();
