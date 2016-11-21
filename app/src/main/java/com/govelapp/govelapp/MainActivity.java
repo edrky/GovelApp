9583baf92530cfceabd7be1fa369672c18e422b7
alevnyaa <alevnyaa@gmail.com>
2016-11-21 17:12:53 +0300

    Got a very barebones UI and also implemented intent to maps activity.

--- app/src/main/java/com/govelapp/govelapp/MainActivity.java
+++ app/src/main/java/com/govelapp/govelapp/MainActivity.java
@@ -1,14 +1,17 @@
 package com.govelapp.govelapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
     private EditText text;
 
     @Override
@@ -16,26 +19,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.search_button);
-        text = (EditText)findViewById(R.id.search_input);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String queryString = text.getText().toString();
-                String[] queryParts = queryString.split(" ");
-
-                //Make REST request hereWatashiWaKai
-
-                //1. Create url from queryParts and actual url
-                //2. http connection to the url, and get answer
-                //3. get map objects
-                //4. create single group of map markers to pass as intentl
-                //for now, let's set some example value on button press
-                queryString = "kahve pasta";
-                text.setText(queryString);
-                //remove upper lines, just for testing
-
-
+        text = (EditText)findViewById(R.id.search_bar);
+        text.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int i, KeyEvent keyEvent) {
+                boolean handled = false;
+                if(i == EditorInfo.IME_ACTION_SEARCH && v.getText().length() > 0){
+                    String query = v.getText().toString();
+                    Intent queryIntent = new Intent(MainActivity.this, MapsActivity.class);
+                    queryIntent.putExtra("query", query);
+                    startActivity(queryIntent);
+                    handled = true;
+                }
+                return handled;
             }
         });
     }
 }
+
+//Make REST request hereWatashiWaKai
+//1. Create url from queryParts and actual url
+//2. http connection to the url, and get answer
+//3. get map objects
+//4. create single group of map markers to pass as intentl
+//for now, let's set some example value on button press
+//queryString = "kahve pasta";
+//text.setText(queryString);
+//remove upper lines, just for testing
